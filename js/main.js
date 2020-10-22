138aba3c0dd016eef3f93ad88c00391b98cff18e
medojo <tochkagaza2@gmail.com>
2020-10-22 14:00:29 +0300

    first commit

---
+++ js/main.js
@@ -0,0 +1,8 @@
+$(function () {
+    'use strict';
+    var winH    = $(window).height(),
+    upperH  = $('.upper-bar').innerHeight(),
+    navH    = $('.navbar').innerHeight();
+    $('.slider, .carousel-item').height(winH - ( upperH + navH ));
+    
+});
